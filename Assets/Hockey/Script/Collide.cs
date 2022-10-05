177a5d2e499096b4a2cb3908c03beba9cfc77f63
JL-EFR <jompop.l@ku.th>
2022-10-05 18:42:20 +0700

    Big Update

--- Assets/Hockey/Script/Collide.cs
+++ Assets/Hockey/Script/Collide.cs
@@ -13,10 +13,11 @@ public class Collide : MonoBehaviour
             float veloX = momemtumX/rb.mass;
             rb.velocity = new Vector3(veloX,0,veloZ);
         }
-        if (coll.collider.name=="Red"){
+        if (coll.collider.tag=="Red"){
+            Debug.Log("Stop");
             rb.velocity = new Vector3(0,0,0);
         }
-        if (coll.collider.name=="Goal"){
+        if (coll.collider.name=="Goal" && rb.name=="Red"){
             Debug.Log("Goal!!!");
         }
     }
--- Assets/Hockey/Script/PlayerMovement.cs
+++ Assets/Hockey/Script/PlayerMovement.cs
@@ -1,18 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    [SerializeField] private Slider SliderV;
+    [SerializeField] private Slider SliderA;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector3(50, 0, 0);
         rb.useGravity = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(rb.position.y>0.15){
+            rb.velocity = new Vector3(rb.velocity.x, -1, rb.velocity.z);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            float veloX = (float)(SliderV.value*Math.Cos(SliderA.value*0.0174533));
+            float veloZ = (float)(SliderV.value*Math.Sin(SliderA.value*0.0174533));
+            rb.velocity = new Vector3(veloX, 0, veloZ);
+        }
     }
 }
